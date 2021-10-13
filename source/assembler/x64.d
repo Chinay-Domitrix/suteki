3c6df7d27fac2c574630382541aa65c9d39f1541
Ryan <ryaangu@gmail.com>
2021-10-13 02:30:38 -0300

    (source/assembler) added and updated some x64 stuff

--- source/assembler/x64.d
+++ source/assembler/x64.d
@@ -1,12 +1,34 @@
 module assembler.x64;
 
+struct X32Register
+{
+    uint index;
+    uint size;
+}
+
 struct X64Register
 {
     uint index;
     uint size;
 }
 
+const X32Register g_eax = { 0, 4 };
+const X32Register g_ecx = { 1, 4 };
+const X32Register g_edx = { 2, 4 };
+const X32Register g_ebx = { 3, 4 };
+const X32Register g_esp = { 4, 4 };
+const X32Register g_ebp = { 5, 4 };
+const X32Register g_esi = { 6, 4 };
+const X32Register g_edi = { 7, 4 };
+
 const X64Register g_rax = { 0, 8 };
+const X64Register g_rcx = { 1, 8 };
+const X64Register g_rdx = { 2, 8 };
+const X64Register g_rbx = { 3, 8 };
+const X64Register g_rsp = { 4, 8 };
+const X64Register g_rbp = { 5, 8 };
+const X64Register g_rsi = { 6, 8 };
+const X64Register g_rdi = { 7, 8 };
 
 struct X64Assembler
 {
@@ -21,8 +43,22 @@ struct X64Assembler
             code ~= bytes[i];
     }
 
+    // XOR
+    void xor(const ref X64Register a, const ref X64Register b)
+    {
+        code ~= 0x48;
+        code ~= 0x31;
+        code ~= cast(ubyte)(((0b11 << 3) | b.index) << 3 | a.index);
+    }
+
+    void xor(const ref X32Register a, const ref X32Register b)
+    {
+        code ~= 0x31;
+        code ~= cast(ubyte)(((0b11 << 3) | b.index) << 3 | a.index);
+    }
+
     // MOV
-    void mov(const ref X64Register register, int value)
+    void mov(const ref X32Register register, int value)
     {
         code ~= cast(ubyte)(0xB8 + register.index);
         write_32(value);
